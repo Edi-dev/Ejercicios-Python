136bdbae01befdb4734149575f57ab8e51e06daa
Edison Puerta <edipuerta714@gmail.com>
2022-03-03 14:57:10 -0500

    Agrego encabezado del ejercicio2

--- ejercico2.py
+++ ejercico2.py
@@ -1,3 +1,5 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+
 nombre = "Edison esta aprendiendo python"
 print (nombre)
 print (type(nombre))
