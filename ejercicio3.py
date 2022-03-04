8590985551953fc6fc54501f3d18041bd36e84a7
Edison Puerta <edipuerta714@gmail.com>
2022-03-03 19:02:49 -0500

    Solucion ejercicio #3

---
+++ ejercicio3.py
@@ -0,0 +1,40 @@
+#Para este ejercicio tenéis que crear en la consola de python variables que representen los siguientes datos de un contacto:
+
+#- Apellidos
+#- Nombre
+#- Edad
+#- Email
+#- Teléfono
+#- Casado (verdadero o falso)
+#- Con Hijos (verdadero o falso)
+#- Lista de amigos
+#- Películas vistas (diccionario con clave y valor. El valor será el título de la película)
+
+
+#Una vez hayas creado todas las variables, muéstralas por consola haciendo uso de la función print().
+
+apellidos = "Puerta Arroyave"
+nombre = "Edison"
+edad = 28
+email = "edipuerta714@gmail.com"
+telefono = 3023566618
+casado = True
+ConHijos = False
+lista = ['Alejandro', 'Julian', 'Plinio', 'Sebastian', 'Pablo']
+peliculas = {
+    'titulo1': 'Los vengadores',
+    'titulo2': 'Transformers',
+    'titulo3': 'Avatar',
+    'titulo4': 'La momia',
+    'titulo5': 'X-Men'
+}
+
+print(apellidos)
+print(nombre)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(ConHijos)
+print(lista)
+print(peliculas)
