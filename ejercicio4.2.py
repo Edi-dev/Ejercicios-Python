8d824aac00fc1536fbe1ed319458800767a07122
Edison Puerta <edipuerta714@gmail.com>
2022-03-07 08:55:53 -0500

    Elimino la variable contador por que no era necesaria.

--- ejercicio4.2.py
+++ ejercicio4.2.py
@@ -2,7 +2,7 @@
 
 # Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
 
-contador = 1
+
 
 for contador in range(2, 8):
     if contador%2 != 0:
